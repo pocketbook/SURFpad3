974c3ad7a6076c38341c989a0236f852c7239327
pocketbook <vladimir.budnik@obreey-products.com>
2014-06-02 18:02:44 +0300

    first commit

---
+++ kernel/init/version.c
@@ -0,0 +1,60 @@
+/*
+ *  linux/init/version.c
+ *
+ *  Copyright (C) 1992  Theodore Ts'o
+ *
+ *  May be freely distributed as part of Linux.
+ */
+
+#include <generated/compile.h>
+#include <linux/module.h>
+#include <linux/uts.h>
+#include <linux/utsname.h>
+#include <generated/utsrelease.h>
+#include <linux/version.h>
+
+#ifndef CONFIG_KALLSYMS
+#define version(a) Version_ ## a
+#define version_string(a) version(a)
+
+extern int version_string(LINUX_VERSION_CODE);
+int version_string(LINUX_VERSION_CODE);
+#endif
+
+struct uts_namespace init_uts_ns = {
+	.kref = {
+		.refcount	= ATOMIC_INIT(2),
+	},
+	.name = {
+		.sysname	= UTS_SYSNAME,
+		.nodename	= UTS_NODENAME,
+		//wisky-lxh@20130704,custom kernel version
+#ifdef WISKY_KERNEL_VERSION
+		.release	= WISKY_KERNEL_VERSION,
+#else
+		.release	= UTS_RELEASE,
+#endif	
+//end-wisky-lxh	
+		.version	= UTS_VERSION,
+		.machine	= UTS_MACHINE,
+		.domainname	= UTS_DOMAINNAME,
+	},
+	.user_ns = &init_user_ns,
+};
+EXPORT_SYMBOL_GPL(init_uts_ns);
+
+/* FIXED STRINGS! Don't touch! */
+const char linux_banner[] =
+		//wisky-lxh@20130704,custom kernel version
+#ifdef WISKY_KERNEL_VERSION
+	"Linux version " WISKY_KERNEL_VERSION " (" LINUX_COMPILE_BY "@"
+#else
+	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
+#endif	
+//end-wisky
+	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+
+const char linux_proc_banner[] =
+	"%s version %s"
+	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (" LINUX_COMPILER ") %s\n";
